eab92fcca619783d82a838c10294402d07b141c5
edenilson <rafaeldenilson1@gmail.com>
2022-09-10 19:45:49 -0300

    criacao do arquivo localizadores.js,configuracao da baseUrl e chromeWebSecurity,criei 2 cenarios um deve realizar login e logout com sucesso,o segundo realiza login e e finaliza um checkout com sucesso

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,45 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+import localizador from '../support/elementos/localizadores'
+
+describe('logar usuario valido', () => {
+
+  beforeEach(() => {
+    cy.visit('/');
+
   })
+
+  it('Deve realizar login e logout com sucesso', () => {
+
+    cy.get(localizador.login.username).type(Cypress.env('user_name'))
+    cy.get(localizador.login.password).type(Cypress.env('password'))
+    cy.get(localizador.login.btn_login).click()
+    cy.get(localizador.inventory.titulo_products).should('have.text', 'Products')
+    cy.get(localizador.Menu.Setting).click()
+    cy.get(localizador.Menu.logout).contains('Logout').click()
+    cy.get(localizador.login.btn_login).should('have.value', 'Login')
+  })
+
+  it('Deve realizar login e finalizar checkout', () => {
+    cy.get(localizador.login.username).type(Cypress.env('user_name'))
+    cy.get(localizador.login.password).type(Cypress.env('password'))
+    cy.get(localizador.login.btn_login).click()
+    cy.get(localizador.inventory.btn_add_cart).click()
+    cy.get(localizador.inventory.btn_carrinho).click()
+    cy.get(localizador.card.btn_checkout).click()
+    cy.get(localizador.checkout.first_name).type('Edenilson')
+    cy.get(localizador.checkout.last_name).type('Rafael')
+    cy.get(localizador.checkout.postal_code).type('08572-450')
+    cy.get(localizador.checkout.btn_continue).click()
+    cy.get(localizador.checkout.btn_finish).click()
+    cy.get(localizador.checkout_complete.text_thank_you_for_your_order).should('have.text', 'THANK YOU FOR YOUR ORDER')
+  })
+  
+  
 })
+
+
+
+
+
+
+
+
---
+++ cypress/support/elementos/localizadores.js
@@ -0,0 +1,54 @@
+const localizador = {
+    login: {
+        username: '[data-test="username"]',
+        password: '[data-test="password"]',
+        btn_login: '[data-test="login-button"]'
+    },
+
+    Menu: {
+        Setting: '#react-burger-menu-btn',
+        logout: 'a'
+    },
+
+    inventory: {
+        titulo_products: '[class="title"]',
+        btn_add_cart: '[data-test="add-to-cart-sauce-labs-backpack"]',
+        btn_carrinho: '[class="shopping_cart_link"]'
+    },
+
+    card: {
+        btn_checkout: '[data-test="checkout"]'
+    },
+
+    checkout: {
+        first_name: '[data-test="firstName"]',
+        last_name: '[data-test="lastName"]',
+        postal_code: '[data-test="postalCode"]',
+        btn_continue: '[data-test="continue"]',
+        btn_finish: '[data-test="finish"]'
+    },
+    checkout_complete: {
+        text_thank_you_for_your_order:'[class="complete-header"]'
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+export default localizador
